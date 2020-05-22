97db66bb64c73adb19139b396825181c3caa28c8
Ezequiel Foncubierta <ezequiel.foncubierta@gmail.com>
2020-05-22 21:46:37 +0200

    Manage runner input parameters with picocli

--- runner/src/main/java/cloudspec/CloudSpecRunCommand.java
+++ runner/src/main/java/cloudspec/CloudSpecRunCommand.java
@@ -25,29 +25,42 @@
  */
 package cloudspec;
 
+import cloudspec.lang.CloudSpec;
+import cloudspec.loader.CloudSpecLoader;
 import picocli.CommandLine;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(
-        name = "cloudspec",
-        mixinStandardHelpOptions = true,
-        version = "0.0.1",
-        subcommands = {
-                CloudSpecRunCommand.class
-        }
+        name = "run",
+        description = "Run CloudSpec tests"
 )
-public class CloudSpecMain implements Callable<Integer> {
+public class CloudSpecRunCommand implements Callable<Integer> {
+    @CommandLine.Option(
+            names = {"-s", "--spec"},
+            paramLabel = "<spec file>",
+            description = "CloudSpec file",
+            required = true
+    )
+    private File specFile;
+
     @Override
     public Integer call() throws Exception {
-        System.out.println("Please, use one of the following commands: ");
-        System.out.println(" - run");
-        System.out.println(" - doc");
-        return null;
-    }
+        InputStream is = new FileInputStream(specFile);
 
-    public static void main(String[] args) {
-        int exitCode = new CommandLine(new CloudSpecMain()).execute(args);
-        System.exit(exitCode);
+        // create loader
+        CloudSpecLoader loader = new CloudSpecLoader();
+
+        // load spec
+        CloudSpec spec = loader.load(is);
+
+        // run spec
+        CloudSpecRunner runner = DaggerCloudSpecRunnerComponent.create().buildCloudSpecRunner();
+        runner.validate(spec);
+
+        return 0;
     }
 }
