f46c9e7a74e6be9a03e48545a92937abafdda4a8
Ezequiel Foncubierta <ezequiel.foncubierta@gmail.com>
2020-07-06 10:05:40 +0200

    Lift IO up to ResourceLoader and run loaders in parallel

--- core/src/main/kotlin/cloudspec/loader/ResourceLoader.kt
+++ core/src/main/kotlin/cloudspec/loader/ResourceLoader.kt
@@ -19,9 +19,14 @@
  */
 package cloudspec.loader
 
-import arrow.core.Option
 import arrow.core.Some
+import arrow.core.extensions.option.monad.flatten
+import arrow.core.extensions.sequence
 import arrow.core.getOrElse
+import arrow.fx.IO
+import arrow.fx.extensions.fx
+import arrow.fx.extensions.io.applicative.applicative
+import arrow.fx.extensions.io.concurrent.parSequence
 import cloudspec.ProvidersRegistry
 import cloudspec.lang.AssociationStatement
 import cloudspec.lang.NestedStatement
@@ -34,129 +39,134 @@ class ResourceLoader(private val providersRegistry: ProvidersRegistry,
                      private val resourceStore: ResourceStore) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    private val loadedResourceDefs = mutableSetOf<ResourceDefRef>()
+    private val loadedResourceDefs = mutableMapOf<ResourceDefRef, IO<Resources>>()
 
     fun load(module: Module) {
         logger.info("Loading resources required to run this test")
 
-        loadFromModule(module)
+        loadFromModule(module).parSequence().unsafeRunSync()
     }
 
-    private fun loadFromModules(modules: List<Module>) {
-        modules.forEach { loadFromModule(it) }
+    private fun loadFromModules(modules: List<Module>): List<IO<*>> {
+        return modules.flatMap { loadFromModule(it) }
     }
 
-    private fun loadFromModule(module: Module) {
-        loadFromModules(module.modules)
-        loadFromRules(module.rules)
+    private fun loadFromModule(module: Module): List<IO<*>> {
+        return loadFromModules(module.modules).plus(loadFromRules(module.rules))
     }
 
-    private fun loadFromRules(rules: List<Rule>) {
-        rules.forEach { loadFromRule(it) }
+    private fun loadFromRules(rules: List<Rule>): List<IO<*>> {
+        return rules.map { loadFromRule(it) }
     }
 
-    private fun loadFromRule(rule: Rule) {
-        // Load all resource definitions
-        getAllResources(rule.configs, rule.defRef)
-            .forEach { resource ->
-                // load dependent resources from each statement
+    private fun loadFromRule(rule: Rule): IO<*> {
+        return IO.fx {
+            // Load all resource definitions
+            val (resources) = getAllResources(rule.configs, rule.defRef)
+            val (statementsIO) = resources.flatMap { resource ->
+                // load dependent resources from each (filter and validation) statement
                 rule.filters
-                    .forEach {
-                        loadFromStatement(rule.configs, resource, it, emptyList())
-                    }
-
-                // load dependent resources from each statement
-                rule.validations
-                    .forEach {
-                        loadFromStatement(rule.configs, resource, it, emptyList())
-                    }
+                    .plus(rule.validations)
+                    .map { loadFromStatement(rule.configs, resource, it, emptyList()) }
             }
+            val (x) = statementsIO.parSequence()
+            x
+        }
     }
 
-    private fun loadFromStatement(sets: SetValues, resource: Resource, statement: Statement, path: List<String>) {
-        when (statement) {
+    private fun loadFromStatement(sets: SetValues, resource: Resource, statement: Statement, path: List<String>): List<IO<*>> {
+        return when (statement) {
             is NestedStatement -> {
-                statement.statements.forEach {
+                statement.statements.flatMap {
                     loadFromStatement(sets, resource, it, path.plus(statement.propertyName))
                 }
             }
             is AssociationStatement -> {
-                loadFromAssociationStatement(sets, resource, statement, path)
+                listOf(loadFromAssociationStatement(sets, resource, statement, path))
             }
-            else -> {
-            }
+            else -> emptyList()
         }
     }
 
     private fun loadFromAssociationStatement(sets: SetValues,
                                              resource: Resource,
                                              statement: AssociationStatement,
-                                             path: List<String>) {
-        val associationPath = path.plus(statement.associationName)
-
+                                             path: List<String>): IO<*> {
         logger.debug("Loading resources for association '${statement.associationName}' " +
                              "in resource '${resource.ref}'")
 
-        when (val associationOpt = resource.getAssociationByPath(associationPath)) {
-            is Some -> {
-                // load associated resource
-                val association = associationOpt.t
-                when (val associatedResourceOpt = getResource(sets, association.resourceRef)) {
-                    is Some ->
-                        // load resources from each sub statement
-                        statement.statements.forEach {
-                            loadFromStatement(sets, associatedResourceOpt.t, it, emptyList())
-                        }
-                    else ->
-                        logger.error("Associated resource '${association.resourceRef}' " +
-                                             "from resource '${resource.ref}' (name = '${statement.associationName}') " +
-                                             "does not exist. Ignoring it.")
+        val associationPath = path.plus(statement.associationName)
 
+        return IO.fx {
+            when (val associationOpt = resource.getAssociationByPath(associationPath)) {
+                is Some -> {
+                    // load associated resource
+                    val association = associationOpt.t
+
+                    val (associatedResourceOpt) = getResource(sets, association.resourceRef)
+                    val (x) = when (associatedResourceOpt) {
+                        is Some -> {
+                            // load resources from each sub statement
+                            statement.statements.flatMap {
+                                loadFromStatement(sets, associatedResourceOpt.t, it, emptyList())
+                            }
+                        }
+                        else -> {
+                            logger.error("Associated resource '${association.resourceRef}' " +
+                                                 "from resource '${resource.ref}' (name = '${statement.associationName}') " +
+                                                 "does not exist. Ignoring it.")
+                            emptyList()
+                        }
+                    }.parSequence()
+                    x
+                }
+                else -> {
+                    logger.error("Association '${statement.associationName}' does not exist " +
+                                         "in resource '${resource.ref}'." +
+                                         "Ignoring it.")
                 }
             }
-            else ->
-                logger.error("Association '${statement.associationName}' does not exist " +
-                                     "in resource '${resource.ref}'." +
-                                     "Ignoring it.")
         }
     }
 
-    private fun getAllResources(sets: SetValues, resourceDefRef: ResourceDefRef): Resources {
-        return if (!loadedResourceDefs.contains(resourceDefRef)) {
+    private fun getAllResources(sets: SetValues, resourceDefRef: ResourceDefRef): IO<Resources> {
+        if (!loadedResourceDefs.containsKey(resourceDefRef)) {
             logger.debug("Loading all resources of type '{}'", resourceDefRef)
 
-            loadedResourceDefs.add(resourceDefRef)
+            loadedResourceDefs[resourceDefRef] = IO.fx {
+                val (resources) = providersRegistry.getProvider(resourceDefRef.providerName)
+                    .map { it.resourcesByDef(sets, resourceDefRef) }
+                    .sequence(IO.applicative())
+                    .map { it.getOrElse { emptyList() } }
 
-            providersRegistry.getProvider(resourceDefRef.providerName)
-                .map { provider ->
-                    provider.resourcesByDef(sets, resourceDefRef).unsafeRunSync()
-                        .onEach { (ref, properties, associations) ->
-                            resourceStore.saveResource(ref, properties, associations)
-                        }
-                }.getOrElse {
-                    emptyList()
+                resources.onEach { (ref, properties, associations) ->
+                    resourceStore.saveResource(ref, properties, associations)
                 }
-        } else {
-            resourceStore.resourcesByDefinition(resourceDefRef)
+            }
         }
+
+        return loadedResourceDefs[resourceDefRef]!!
     }
 
-    private fun getResource(sets: SetValues, ref: ResourceRef): Option<Resource> {
+    private fun getResource(sets: SetValues, ref: ResourceRef): IO<ResourceO> {
         // TODO load incomplete resource from store
 //        Optional<Resource> resourceOpt = resourceStore.getResource(resourceDefRef, resourceId);
 //        if (!resourceOpt.isPresent()) {
         logger.debug("Loading resource of type '${ref}'")
 
-        return providersRegistry.getProvider(ref.defRef)
-            .flatMap { provider ->
-                provider.resource(sets, ref).unsafeRunSync()
-                    .also {
-                        // if resource exists, save it
-                        if (it is Some) {
-                            resourceStore.saveResource(ref, it.t.properties, it.t.associations)
-                        }
-                    }
+        return IO.fx {
+            val (resource) = providersRegistry.getProvider(ref.defRef)
+                .map { provider ->
+                    provider.resource(sets, ref)
+                }
+                .sequence(IO.applicative())
+                .map { it.flatten() }
+            resource.also {
+                if (it is Some) {
+                    resourceStore.saveResource(ref, it.t.properties, it.t.associations)
+                }
             }
+        }
 
         //        }
 //        return resourceOpt;
